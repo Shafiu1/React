4ed6d457956b4eeafc66caf5f8f6503438c85ed1
Shafiul Alam <shafiulalam0307@gmail.com>
2025-01-17 21:38:59 +0600

    rendering object instead of function

--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -2,8 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <MyApp/>
+    anotherElement;
   </StrictMode>,
 )
