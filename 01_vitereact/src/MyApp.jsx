669a299773cfabf84873bdfed5505daf9a9c4e0a
Shafiul Alam <shafiulalam0307@gmail.com>
2025-01-19 08:13:39 +0600

    debug the custom element rendering in react

---
+++ 01_vitereact/src/MyApp.jsx
@@ -0,0 +1,9 @@
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App! Shafiul Alam</h1>
+    </div>
+  )
+}
+
+export default MyApp;
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,22 +1,28 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-function MyApp(){
-  return(
-    <div>
-      <h1>Custom App!</h1>
-    </div>
-  )
-}
+import MyApp from './MyApp.jsx'
 
-const anotherElement = (
-  <a href='https://google.com' target='_blank'>Visit Google</a>
+
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href:"https://google.com",target:'_blank'
+  },
+  'click me to visit google'
 )
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <MyApp/>
-    anotherElement;
+    {AnotherElement}<br/>
+    {/* //for variable used the currly breces. the tutorial didn't show you that. */}
+    {reactElement}
   </StrictMode>,
 )
