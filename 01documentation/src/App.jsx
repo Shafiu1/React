2a1a408c478e0e61bfa8ef89693e9e8e07f3cfc5
Shafiul Alam <shafiulalam0307@gmail.com>
2025-01-25 20:57:37 +0600

    getting the knowledge of lifting state up

--- 01documentation/src/App.jsx
+++ 01documentation/src/App.jsx
@@ -1,13 +1,14 @@
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
-import ListRendering from './components/ListRendering.jsx'
 import RespondEvent from './components/RespondEvent.jsx'
+import ListRendering from './components/ListRendering.jsx'
 
 
 function App() {
-  
+  const [count,setCount]=useState(0);
   return (
     <>
       <ListRendering/>
